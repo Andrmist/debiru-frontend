aebe1b49c7b006fecd018abfed4b853f8c655456
Andrmist <andr2334@gmail.com>
2021-01-22 22:14:05 +0200

    Фикс библиотеки рендера скинов

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,9 @@
 import React, { createRef, useEffect } from 'react';
 import styled from 'styled-components';
 
-const Canvas = styled.canvas({
+import SkinRender from '../../lib/MineRender/skin/index';
+
+const Canvas = styled.div({
 	width: 500,
 	height: 500,
 	background: '#aaa',
@@ -10,9 +12,8 @@ const Canvas = styled.canvas({
 export default function () {
 	const canvas = createRef();
 	useEffect(() => {
-		//
-		// canvas.xyemae
-		//
+		let skinRender = new SkinRender({/* options */}, canvas.current);
+		skinRender.render("Qugalego");
 	});
 	return <Canvas ref={canvas} />;
 }
